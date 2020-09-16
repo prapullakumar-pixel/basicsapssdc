049b05058a66549c49083370668e1198f11644d5
prapullakumar <prapulla3593@gmail.com>
2020-09-16 11:06:31 +0530

    added

--- dynamic.js
+++ dynamic.js
@@ -12,7 +12,7 @@
      xhr.open("GET",file,true);
      xhr.onreadystatechange = ()=>{
         if (xhr.readyState =="4" && xhr.status ===200) {
-           callback(xhr.responseText);
+        callback(xhr.responseText);
         }
      }
     xhr.send(); 
@@ -36,11 +36,10 @@ function basic_details(SDC) {
      var n = document.createElement("h1");
      n.textContent=SDC.name;
      main.appendChild(n);
-var a = document.createElement("p");
-a.classList.add("age");
-a.setAttribute("id","age-id");
-a.textContent=SDC.age;
-main.appendChild(a);
+
+     var a = document.createElement("p");
+     a.setAttribute("id","age-id");
+     main.appendChild(a);
 
 var h = document.createElement("h1");
 h.textContent="Courses in Apssdc";
@@ -57,32 +56,19 @@ main.appendChild(ul);
 }
 
 function trainers_details(t) {
-var h = document.createElement("h1");
-h.textContent="Trainers details in Apssdc";
-main.appendChild(h);
-h.appendChild(document.createElement("hr")); 
+   var h = document.createElement("h1");
+   h.textContent="Trainers details in Apssdc";
+   main.appendChild(h);
+   h.appendChild(document.createElement("hr")); 
 
 var table = document.createElement("table");
 var row="";
 for(var i in t){
-row +="<tr><td>"+t[i].name+"</td><td>"+t[i].phoneNo+"</td><td>"+t[i].Email+"</td></tr>";    
+row +="<tr><td>"+t[i].name+"</td><td>"+t[i].phoneNo+"</td><td>"+t[i].Email+"</td><td>"+t[i].course+"</td></tr>";    
 }
 table.innerHTML=row;
 main.appendChild(table);
 }
 document.write("<br>")
 
-class Square{
-  constructor(height,width){
-        this.h=height;
-        this.w=width;
-  }
-}
-var a=new Square("40cm","40cm");
-document.write(a.w);
-document.write("<br>")
 
-function add(p1,p2){
- return(p1+p2)
-}
-add(1,2,3,4,5)
